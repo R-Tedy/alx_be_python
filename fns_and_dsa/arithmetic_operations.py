aae0353ba512856f2eb639f36db8be80f7ee0620
R-Tedy <stedyroland2@gmail.com>
2025-06-14 09:00:33 +0300

    arithmetic operation functions

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+def perform_operation(num1, num2, operation):
+  match operation:
+    case 'add':
+      result = num1 + num2
+      return result
+    case 'subtract':
+      result = num1 - num2
+      return result
+    case 'multiply':
+      result = num1 * num2
+      return result
+    case 'divide':
+      if num2 == 0:
+        return 'Kindly input a valid operation.'
+      else:
+        result = num1 / num2
+        return result
